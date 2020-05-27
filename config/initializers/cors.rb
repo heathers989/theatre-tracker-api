0492058b000f78204ab2df5ce21feaa3b5575402
Heather Spoelstra <hmspoelstra@gmail.com>
2020-05-26 23:27:24 -0400

    fixed cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://theatre-tracker-api.herokuapp.com'
+    origins 'https://theatre-tracker.herokuapp.com'
 
     resource '*',
       headers: :any,
